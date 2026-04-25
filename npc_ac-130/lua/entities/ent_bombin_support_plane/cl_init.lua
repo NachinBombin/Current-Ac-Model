fedf687bfeb70f1dadbaf9133e39af3149089a42
NachinBombin <solidnachox@gmail.com>
2026-04-24 22:32:46 -0400

    Fix: impact sounds silent - sound.Play requires sound/ prefix

--- npc_ac-130/lua/entities/ent_bombin_support_plane/cl_init.lua
+++ npc_ac-130/lua/entities/ent_bombin_support_plane/cl_init.lua
@@ -1,11 +1,12 @@
 include("shared.lua")
 
--- Receive gunfire / pass sound broadcast from server and play locally
+-- sound.Play requires paths relative to the game root (i.e. prefixed with "sound/").
+-- All paths sent over the net are stored without that prefix, so we add it here.
 net.Receive("bombin_plane_sound", function()
     local path   = net.ReadString()
     local pos    = net.ReadVector()
     local level  = net.ReadUInt(8)
     local pitch  = net.ReadUInt(8)
     local volume = net.ReadFloat()
-    sound.Play(path, pos, level, pitch, volume)
+    sound.Play("sound/" .. path, pos, level, pitch, volume)
 end)
