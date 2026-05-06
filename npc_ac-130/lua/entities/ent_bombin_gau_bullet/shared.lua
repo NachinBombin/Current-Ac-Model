ce075eed3b3f2f20b1b7d2b4891ae6ac4cd30d53
NachinBombin <solidnachox@gmail.com>
2026-05-06 02:13:33 -0400

    Delete npc_ac-130 directory

--- npc_ac-130/lua/entities/ent_bombin_gau_bullet/shared.lua
+++
@@ -1,4 +0,0 @@
-ENT.Type     = "anim"
-ENT.Base     = "base_anim"
-ENT.PrintName = "Bombin GAU Bullet"
-ENT.Spawnable = false
--- npc_ac-130/lua/traj_gau/firebullets.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile()
-
--- The EntityFireBullets hook is NOT used by the AC-130 (traj_gau_broadcast is
--- called directly from FireGAUBulletAt in init.lua). Hook disabled to avoid
--- interfering with other addons.
-traj_gau_disable_hook = true
-
-print("[TrajGAU] firebullets loaded.")
